576ccf01c04922e90fc0e42ee7281870b1896969
Shloka-Patel <137433503+Shloka-Patel@users.noreply.github.com>
2024-08-19 15:28:55 +0530

    Add files via upload

---
+++ Shloka_B2_120_Exp9A_CDS.cpp
@@ -0,0 +1,29 @@
+// NAME - SHLOKA PATEL 
+// PRN - 23070123120 
+// EXPERIMENT - 9(A) 
+
+// Program to illustrate pointers. 
+
+#include <bits/stdc++.h> 
+using namespace std;
+void geeks()
+{
+    int var = 5;
+
+    int* ptr;                  // Declaring pointer variable. 
+
+    ptr = &var;
+
+    cout<<"Value at ptr = "<<ptr<<"\n";
+    cout<<"Value at var = " <<var<<"\n";
+    cout<<"Value at *ptr = "<<*ptr<<"\n";
+
+}
+
+// Driver Program 
+
+int main()
+{
+    geeks();
+    return 0;
+} 
